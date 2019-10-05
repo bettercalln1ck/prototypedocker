e22bfa119a574843e719f6764b66681ae4316c54
nikolai6303 <nikbmw007@gmail.com>
2019-10-06 03:06:57 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+   define('DB_USERNAME', 'nikhil');
+   define('DB_PASSWORD', 'python');
+   define('DB_DATABASE', 'nikdata');
+   $db = mysqli_connect(DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
---
+++ docker images/source/flag.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+
+  function get_mad_and_give_flag() {
+	  die("What did you just say to me?!?! JCTF{KALA_SABKI_LEGA}");
+  }
+?>
+</body>
